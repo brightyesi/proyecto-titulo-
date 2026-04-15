82676175a0d764479ce132c5bc1b113065c73bc9
brightyesi <yes.jara@duocuc.cl>
2026-04-15 12:13:53 -0400

    commit inicial

---
+++ src/main/java/Proceso_Administrativo/proyecto_titulo/ProyectoTituloApplication.java
@@ -0,0 +1,13 @@
+package Proceso_Administrativo.proyecto_titulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTituloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoTituloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Proceso_Administrativo/proyecto_titulo/ProyectoTituloApplicationTests.java
@@ -0,0 +1,13 @@
+package Proceso_Administrativo.proyecto_titulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTituloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
