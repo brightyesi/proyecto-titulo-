cac9b704444672c4c18a645346056fb1acc6db6a
Cris2427 <alonso.ctp@gmail.com>
2026-06-25 10:55:49 -0400

    Swagger con autenticacion JWT, OpenAPI implementado

---
+++ src/main/java/Proceso_Administrativo/proyecto_titulo/Config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package Proceso_Administrativo.proyecto_titulo.Config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API - Sistema de Gestión de Facturas Agrícolas",
+                version = "1.0",
+                description = "Documentación de la API REST del backend"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
